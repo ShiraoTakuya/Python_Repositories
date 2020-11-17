f3d1597f9803291937d06fd26d24ebec2cf09bdf
ShiraoTakuya <shirao_genu@yahoo.co.jp>
2020-11-17 21:44:04 +0900

    Added new project

---
+++ Oscilloscope/Run.cmd
@@ -0,0 +1,3 @@
+cd /d %~dp0
+python source.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+PAUSE
---
+++ Oscilloscope/source.py
@@ -0,0 +1,25 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# Definition of initial parameters for waveform
+t_s = np.arange(0,1,0.01)
+sig_V = np.zeros(100)
+omega = 2*np.pi*1 #f = 1Hz
+
+# Initialization of plt
+fig = plt.figure()
+axs = [fig.add_subplot(111)]
+
+def update(t):
+	print(t) # 2 zeros occur. I don't know why it occurs.
+	axs[0].cla()
+	axs[0].set_xlim([0, 1])
+	axs[0].set_ylim([-1, 1])
+	sig_V[:] = np.append(np.sin(omega*t),sig_V)[0:-1]
+	axs[0].plot(t_s, sig_V, c="blue")
+
+anim = FuncAnimation(fig, update, frames=t_s, interval=10)
+
+anim.save("oscilloscope.gif", writer = 'imagemagick')
+plt.show()
